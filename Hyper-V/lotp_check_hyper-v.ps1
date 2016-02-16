bf27e8965eae99e8f0b5dcc94f6e0de219318d9a
mathieuchateau <mathieu.chateau@lotp.fr>
2016-02-16 22:35:42 +0100

    Fic check_hyper-v for non english servers
    Credits to Alexander Koch

--- Hyper-V/lotp_check_hyper-v.ps1
+++ Hyper-V/lotp_check_hyper-v.ps1
@@ -2,7 +2,7 @@
 # Check Hyper-V VM state
 # Author: Mathieu Chateau - LOTP
 # mail: mathieu.chateau@lotp.fr
-# version 0.1
+# version 0.2
 # ====================================================================
 
 #
@@ -19,6 +19,21 @@ param
 	[int]$maxWarn = 1,
 	[int]$maxCrit = 5
 )
+
+
+Function Using-Culture (
+[System.Globalization.CultureInfo]$culture,
+[ScriptBlock]$script)
+{
+    $OldCulture = [System.Threading.Thread]::CurrentThread.CurrentCulture
+    trap
+    {
+    [System.Threading.Thread]::CurrentThread.CurrentCulture = $OldCulture
+    }
+    [System.Threading.Thread]::CurrentThread.CurrentCulture = $culture
+    $ExecutionContext.InvokeCommand.InvokeScript($script)
+    [System.Threading.Thread]::CurrentThread.CurrentCulture = $OldCulture
+} # End Function
 
 # check that powershell Hyper-V module is present
 if(Get-Module -Name "Hyper-V" -ListAvailable)
@@ -45,52 +60,54 @@ $allVM=get-VM
 $output=""
 $excludeVMArray=@()
 # check params if provided
-if($excludeVM -ne "")
+if($excludeVM -ne ""
 {
 	$excludeVMArray=$excludeVM -split ","
 }
+Using-Culture en-us {
+    foreach ($vm in $allVM)
+    {
+	    if ($excludeVMArray -notcontains $vm.Name)
+	    {
+		    if($vm.State -ne [Microsoft.HyperV.PowerShell.VMState].GetEnumName(2))
+		    {
+			    $countVMNotRunning++
+		    }
+		    if($vm.Status -ne "Operating normally")
+		    {
+			    $countVMIssue++
+		    }
+	    }
+    }
 
-foreach ($vm in $allVM)
-{
-	if ($excludeVMArray -notcontains $vm.Name)
-	{
-		if($vm.State -ne "Running")
-		{
-			$countVMNotRunning++
-		}
-		if($vm.Status -ne "Operating normally")
-		{
-			$countVMIssue++
-		}
-	}
+    $bpaResult=Get-BpaResult -ModelId Microsoft/Windows/Hyper-V -ErrorAction 'silentlycontinue' | ? {$_.Resolution -ne $null}
+    Start-Job -Name "Refresh BPA" -ScriptBlock { Invoke-BpaModel -ModelId Microsoft/Windows/Hyper-V} |Out-Null
+
+    $countBPAWarn=($bpaResult |? {$_.Severity -eq "Warning"}).Count
+    $countBPACrit=($bpaResult |? {$_.Severity -eq "Error"}).Count
+
+    if(($countBPACrit -gt $maxCrit) -or ($countVMNotRunning -gt $maxCrit) -or ($countVMIssue -gt $maxCrit))
+    {
+	    $state="CRITICAL"
+	    $exitcode=2
+    }
+    elseif(($countBPAWarn -gt $maxWarn) -or ($countVMNotRunning -gt $maxWarn) -or ($countVMIssue -gt $maxWarn))
+    {
+	    $state="WARNING"
+	    $exitcode=1
+    }
+    else
+    {
+	    $state="OK"
+	    $exitcode=0
+    }
+
+    $output=$state+": "+"'VM Not Running:'"+$countVMNotRunning+" "+"'VM issues:'"+$countVMIssue+" "+"BPA Error:"+$countBPACrit+" "+"BPA Warning:"+$countBPAWarn
+    $output+="|"+"VM_Not_Running="+$countVMNotRunning+" "
+    $output+="VM_issues="+$countVMIssue+" "
+    $output+="BPA_Error="+$countBPACrit+" "
+    $output+="BPA_Warning="+$countBPAWarn
+
+    Write-Host $output
+    exit $exitcode
 }
-
-$bpaResult=Get-BpaResult -ModelId Microsoft/Windows/Hyper-V -ErrorAction 'silentlycontinue' | ? {$_.Resolution -ne $null}
-Start-Job -Name "Refresh BPA" -ScriptBlock { Invoke-BpaModel -ModelId Microsoft/Windows/Hyper-V} |Out-Null
-
-$countBPAWarn=($bpaResult |? {$_.Severity -eq "Warning"}).Count
-$countBPACrit=($bpaResult |? {$_.Severity -eq "Error"}).Count
-
-if(($countBPACrit -gt $maxCrit) -or ($countVMNotRunning -gt $maxCrit) -or ($countVMIssue -gt $maxCrit))
-{
-	$state="CRITICAL"
-	$exitcode=2
-}
-elseif(($countBPAWarn -gt $maxWarn) -or ($countVMNotRunning -gt $maxWarn) -or ($countVMIssue -gt $maxWarn))
-{
-	$state="WARNING"
-	$exitcode=1
-}
-else
-{
-	$state="OK"
-	$exitcode=0
-}
-
-$output=$state+": "+"'VM Not Running:'"+$countVMNotRunning+" "+"'VM issues:'"+$countVMIssue+" "+"BPA Error:"+$countBPACrit+" "+"BPA Warning:"+$countBPAWarn
-$output+="|"+"VM_Not_Running="+$countVMNotRunning+" "
-$output+="VM_issues="+$countVMIssue+" "
-$output+="BPA_Error="+$countBPACrit+" "
-$output+="BPA_Warning="+$countBPAWarn
-Write-Host $output
-exit $exitcode
